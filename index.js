151e9d44ce57cdbbd2fcca5e61ba23f6f8d63f77
Max Witt <kirayatail@gmail.com>
2020-03-21 21:41:32 +0100

    Update default device name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 var util  = require('util');
 
 // Edit this value to point to your device name
-var deviceName = 'Main Room'
+var deviceName = 'Linn Main Room'
 
 var device = {};
 
